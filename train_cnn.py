a40ef1280bee71ac64a49329ecd496bb318ba498
clementschmitt <clement.flance@gmail.com>
2026-04-06 00:52:58 +0200

    Architecture CNN train_cnn.py

--- train_cnn.py
+++ train_cnn.py
@@ -27,3 +27,13 @@ X = np.array(images)
 X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.30, random_state=42)
 X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.50, random_state=42)
 
+model_cnn = keras.Sequential([keras.layers.Input(shape=(224,224,3)),
+                  keras.layers.Conv2D(32, kernel_size=3, activation="relu"),
+                  keras.layers.MaxPooling2D(pool_size=2),
+                  keras.layers.Conv2D(64, kernel_size=3, activation="relu"),
+                  keras.layers.MaxPooling2D(pool_size=2),
+                  keras.layers.Flatten(),
+                  keras.layers.Dense(64, activation="relu"),
+                  keras.layers.Dropout(0.5),
+                  keras.layers.Dense(4, activation="softmax")
+                  ])
